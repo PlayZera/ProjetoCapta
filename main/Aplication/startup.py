ab8f140cb3409d08c0b164b12ab46a1171b5f741
PlayZera <juninhomacedo2010@hotmail.com>
2022-03-13 15:10:28 -0300

    ajustes pontuais

---
+++ main/Aplication/startup.py
@@ -0,0 +1,5 @@
+from main.Domain.RNA_capture import Teste
+
+run = Teste()
+
+run.testeChrome()
--- main/Domain/RNA_capture.py
+++ main/Domain/RNA_capture.py
@@ -2,10 +2,15 @@ import time
 
 from selenium import webdriver
 
-driver = webdriver.Chrome('../WebDriver/chromedriver.exe')  # Optional argument, if not specified will search path.
 
-driver.get('http://www.google.com/');
+class Teste():
 
-time.sleep(10)
+    def testeChrome():
 
-driver.quit()
+        driver = webdriver.Chrome('../WebDriver/chromedriver.exe')
+
+        driver.get('http://www.google.com/')
+
+        time.sleep(10)
+
+        driver.quit()
